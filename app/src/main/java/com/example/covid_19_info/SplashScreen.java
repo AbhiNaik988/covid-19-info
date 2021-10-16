5295816d9c4102c4af844381d05975c198309608
AbhiNaik988 <abhinaik988@gmail.com>
2021-10-16 15:28:28 +0530

    added icon and splash screen

---
+++ app/src/main/java/com/example/covid_19_info/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.covid_19_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
